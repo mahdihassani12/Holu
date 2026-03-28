12b8cc383192e3e7531472fe3a1cddcd5f7420e1
Mahdi Hassani <hassanim430@gmail.com>
2026-03-28 10:33:28 +0430

    fixing logout issue

--- www/_logout.php
+++ www/_logout.php
@@ -1,9 +1,11 @@
 <?php
-	session_start();
-	if(isset($_SESSION['holu_users_id']) AND isset($_SESSION['holu_username'])){
-		session_unset($_SESSION['holu_users_id']);
-		session_unset($_SESSION['holu_username']);
-		header("location:../login.php");
-		exit();
-	}
+session_start();
+
+if (isset($_SESSION['holu_users_id']) && isset($_SESSION['holu_username'])) {
+    unset($_SESSION['holu_users_id']);
+    unset($_SESSION['holu_username']);
+    
+    header("location: ../login.php");
+    exit();
+}
 ?>
