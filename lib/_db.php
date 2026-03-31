66e6e8b7220537a50d40e238963877f7112e5873
Mahdi Hassani <hassanim430@gmail.com>
2026-03-31 09:36:45 +0430

    renaming the db to flow book

--- lib/_db.php
+++ lib/_db.php
@@ -1,6 +1,6 @@
 <?php
 	date_default_timezone_set('Asia/Kabul');
 	
-	$db = new PDO("mysql:host=localhost;dbname=holu;charset=utf8","root","");	
+	$db = new PDO("mysql:host=localhost;dbname=flowbook;charset=utf8","root","");	
 
 ?>
